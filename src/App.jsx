1487282dab11c828b87b6e01888898d9880c120f
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-20 18:41:07 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function App() {
         <div className="App">
             <MyNav home="home" about="Abous Us" browse="Find Us" />
             <Welcome welcome="Benvenuto nella libreria super Top!" />
-            {/* <BookListComponent books={SciFiBooks} /> */}
-            <FormComponent />
+            {/*  <BookListComponent books={SciFiBooks} /> */}
+            {/*  <FormComponent /> */}
             <AllTheBooks />
             <MyFooter testo_par="questo è del testo passato tramite props" />
         </div>
